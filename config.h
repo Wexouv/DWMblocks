cfc5ec2e5c18cd9ecf0ea8ca268b786bdbf7752f
Alex Wexov <wexouv@gmail.com>
2022-01-11 14:32:47 +0500

    First commit !

---
+++ config.h
@@ -0,0 +1,9 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+    /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+    {"", "clock", 1, 0},
+    {"", "battery", 300, 0}
+};
+
+//Sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char *delim = "|";
