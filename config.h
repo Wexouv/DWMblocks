bb5f3ea6b5318d3dbfca7b7339fef63bd9ac9d6b
Alex Wexov <wexouv@gmail.com>
2022-02-07 16:36:36 +0500

    Added CPU temp information

--- config.h
+++ config.h
@@ -1,10 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"", "clock", 1, 0},
+    {"", "clock",    1,  0},
     {"", "net_name", 60, 0},
-    {"", "netspeed", 3, 0},
-    {"", "battery", 300, 0}
+    {"", "netspeed", 3,  0},
+    {"", "cpu_temp", 5,  0},
+    {"", "battery",  300,0},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
